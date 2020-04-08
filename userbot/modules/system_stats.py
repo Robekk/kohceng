c9d23acf164dcf61b6445cdfc2f39b5c8c518314
Robekk <46304722+Robekk@users.noreply.github.com>
2020-04-08 08:18:31 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -122,13 +122,14 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .on command, check if the bot is running.  """
-    await alive.edit("I'm alive...\n"
-                     "and ready to the rock!!!!\n"
-                     f"------------------------------------\n"
-                     f"•  User             : [ᶦᶰᵈ᭄✿Ｒｅｎｃｏ࿐](https://t.me/RobekGANS)\n"
-                     f"•  Python           : {python_version()}\n"
-                     f"•  Telethon version : {version.__version__}\n"
-                     f"------------------------------------\n")
+    await alive.edit(
+"I'm alive...\n"
+"and ready to the rock!!!!\n"
+f"------------------------------------\n"
+f"•  User             : [ᶦᶰᵈ᭄✿Ｒｅｎｃｏ࿐](https://t.me/RobekGANS)\n"
+f"•  Python           : {python_version()}\n"
+f"•  Telethon version : {version.__version__}\n"
+f"------------------------------------\n")
 
 
 @register(outgoing=True, pattern="^.aliveu")
